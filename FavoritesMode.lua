a42022edfd425dc8c7c9718993b0119139893fb1
FireGiraffe <firegiraffegaming@gmail.com>
2026-09-21 11:12:05 -0500

    Update FavoritesMode.lua

--- FavoritesMode.lua
+++ FavoritesMode.lua
@@ -79,11 +79,11 @@ local function spawnThreat()
 			Asset = "https://raw.githubusercontent.com/FireGiraffe/Favorites-Mode-Doors/main/Threat.rbxm",
 			HeightOffset = 0
 		},
-		Movement = {Speed = 225, Delay = 1, Reversed = false},
+		Movement = {Speed = 425, Delay = 1, Reversed = false},
 		Damage = {Enabled = true, IgnoreHiding = false, Range = 45, Amount = 125},
 		Rebounding = {Enabled = true, Type = "Ambush", Min = 5, Max = 8, Delay = 2},
 		Lights = {Flicker = {Enabled = true, Duration = 1}, Shatter = false, Repair = false},
-		Earthquake = {Enabled = true},
+		Earthquake = {Enabled = false},
 		CameraShake = {Enabled = true, Values = {1.8, 75, 0.1, 1}, Range = 120},
 		Crucifixion = {Type = "Curious", Enabled = true, Range = 40, Resist = false, Break = true},
 		Death = {
@@ -159,7 +159,7 @@ local function spawnRipper()
 			Damage = {Enabled = true, IgnoreHiding = false, Range = 50, Amount = 125},
 			Rebounding = {Enabled = false, Type = "Ambush", Min = 1, Max = 1, Delay = 2},
 			Lights = {Flicker = {Enabled = false}, Shatter = false, Repair = false},
-			Earthquake = {Enabled = true},
+			Earthquake = {Enabled = false},
 			CameraShake = {Enabled = true, Values = {2.5, 30, 0.1, 1}, Range = 140},
 			Crucifixion = {Type = "Curious", Enabled = true, Range = 40, Resist = false, Break = true},
 			Death = {
